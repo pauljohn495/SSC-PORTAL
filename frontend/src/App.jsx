0224376f348dfdd601603abad36b8a7072420d6a
pauljohn495 <johnpaultagalog@Gmail.com>
2025-12-01 15:48:07 +0800

    Minor Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import Home from './pages/Home'
